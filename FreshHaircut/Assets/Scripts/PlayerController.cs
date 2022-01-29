9b7db39cd6b28e951e391975083eb2178693d696
Tove Backenroth <tovebac@hotmail.com>
2022-01-29 14:33:56 +0100

    movement is hard

--- FreshHaircut/Assets/Scripts/PlayerController.cs
+++ FreshHaircut/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+	public float accelerationValue = 10f;
+	public float maxMovementSpeed = 20f;
+
+	private Rigidbody _rigidbody;
+	private Vector2 _currentInputs;
+	private Vector3 _currentVelocity;
 	private MouseInputController InputController = null;
 
 	private Camera CurrentCamera = null;
@@ -11,18 +19,29 @@ public class PlayerController : MonoBehaviour
 	{
 		InputController = new MouseInputController();
 		CurrentCamera = Camera.main;
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		SolveVelocity();
+		DoTheMove();
 	}
 
 	private void OnEnable()
 	{
-		InputController.PlayerOne.ConfirmMovementPoint.performed += OnSetWaypoint;
+		// InputController.PlayerOne.ConfirmMovementPoint.performed += OnSetWaypoint;
+		InputController.PlayerOne.Move.performed += OnSetMovementInput;
+		InputController.PlayerOne.Move.canceled += OnSetMovementInput;
 		
 		InputController.Enable();
 	}
 
 	private void OnDisable()
 	{
-		InputController.PlayerOne.ConfirmMovementPoint.performed -= OnSetWaypoint;
+		//InputController.PlayerOne.ConfirmMovementPoint.performed -= OnSetWaypoint;
+		InputController.PlayerOne.Move.performed -= OnSetMovementInput;
+		InputController.PlayerOne.Move.canceled -= OnSetMovementInput;
 		
 		InputController.Disable();
 	}
@@ -30,5 +49,38 @@ public class PlayerController : MonoBehaviour
 	private void OnSetWaypoint(InputAction.CallbackContext obj)
 	{
 		// Vector3 worldMousePosition = CurrentCamera.ScreenToWorldPoint()
+	}
+
+	private void OnSetMovementInput(InputAction.CallbackContext obj)
+	{
+		_currentInputs = obj.ReadValue<Vector2>();
+	}
+
+	private void DoTheMove()
+	{
+		transform.position += _currentVelocity * Time.deltaTime;
+	
+		//_rigidbody.AddForce(_currentVelocity);
+		//_rigidbody.velocity = _currentVelocity;
+	}
+
+	private void SolveVelocity()
+	{
+
+		Vector3 yAxis = Vector3.ProjectOnPlane( CurrentCamera.transform.forward, Vector3.up );
+		Vector3 xAxis = Vector3.Cross( Vector3.up, yAxis );
+		
+		Debug.DrawLine(transform.position, transform.position + xAxis);
+		
+		_currentVelocity += yAxis * _currentInputs.y * accelerationValue * Time.deltaTime;
+		_currentVelocity += xAxis * _currentInputs.x * accelerationValue * Time.deltaTime;
+		
+		_currentVelocity.x = Mathf.Clamp(_currentVelocity.x, -maxMovementSpeed, maxMovementSpeed);
+		_currentVelocity.y = Mathf.Clamp(_currentVelocity.y, -maxMovementSpeed, maxMovementSpeed);
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawLine(transform.position, transform.position + new Vector3(_currentVelocity.x, _currentVelocity.y));
 	}
 }

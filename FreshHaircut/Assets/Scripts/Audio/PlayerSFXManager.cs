f80a8ff12f8e690d35c5bb4e06ee6dc29357b7cd
Tove Backenroth <tovebac@hotmail.com>
2022-01-29 21:12:54 +0100

    footsteps

--- FreshHaircut/Assets/Scripts/Audio/PlayerSFXManager.cs
+++ FreshHaircut/Assets/Scripts/Audio/PlayerSFXManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -7,10 +8,37 @@ public class PlayerSFXManager : MonoBehaviour
     [SerializeField] private AudioSource _sfxSource;
     [SerializeField] private AudioSource _scSfxSource;
     [SerializeField] private AudioClip _exhale;
+    [SerializeField] private AudioClip _footstep;
+
+    private Coroutine _lastRoutine = null;
 
     public void Exhale()
     {
         _scSfxSource.clip = _exhale;
         _scSfxSource.Play();
+    }
+
+    public void Walk(bool isWalking)
+    {
+        if (isWalking == true)
+        {
+            _sfxSource.clip = _footstep;
+            _lastRoutine = StartCoroutine(Walking());
+        }
+        else
+        {
+            StopCoroutine(_lastRoutine);
+        }
+    }
+
+    private IEnumerator Walking()
+    {
+        while (true)
+        {
+            _sfxSource.pitch = Random.Range(0.97f, 1.02f);
+            _sfxSource.volume = Random.Range(0.2f, 0.4f);
+            _sfxSource.Play();
+            yield return new WaitForSeconds(0.4f);
+        }
     }
 }

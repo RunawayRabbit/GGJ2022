fd82c4b5e94411eab5a5e8962b7d7025de840c30
ejt <RunawayRabbitGames@gmail.com>
2022-01-30 16:02:03 +0100

    LookAtPlayer fixed

--- FreshHaircut/Assets/Scripts/LookAtPlayer.cs
+++ FreshHaircut/Assets/Scripts/LookAtPlayer.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour
 {
-	private GameObject _player;
+	private static GameObject _player;
 	private Vector3 _startDir;
+
 	private void Awake()
 	{
-		_player  = GameObject.FindWithTag( "Player" );
+		if( !_player )
+		{
+			_player   = GameObject.FindWithTag( "Player" );
+		}
 		_startDir = transform.forward;
+
 	}
 
-	void Update()
+	private void Update()
 	{
-		var targetLocation = _player.transform.position + Vector3.up;
-		if(Vector3.Dot( _startDir,
-						(_startDir - targetLocation).normalized) > 0.65f)
-
-		{
-			transform.LookAt(targetLocation, Vector3.up);
-		}
-    }
+		var targetLocation = _player.transform.position;
+		if(Vector3.Dot( _startDir, (targetLocation - transform.position ).normalized ) > 0.65f)
+			transform.LookAt(targetLocation + Vector3.down, Vector3.up);
+	}
 }
